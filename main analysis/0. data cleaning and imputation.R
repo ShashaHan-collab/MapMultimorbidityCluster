a7e1d9dc1588453432814ac064df856384a8e072
haonan925 <63240147+haonan925@users.noreply.github.com>
2024-06-04 12:40:28 +0800

    Add files via upload

--- main analysis/0. data cleaning and imputation.R
+++ main analysis/0. data cleaning and imputation.R
@@ -1,238 +1,166 @@
-
 library(dplyr)
 library(data.table)
 library(stringr)
+library(parallel)
+library(readxl)
 library(openxlsx)
-library("readxl")
+library(mice)
 
 setwd("~/")
 
 ## auxliary functions
-std_date <- function(x){
-  if (is.na(x)) return(NA)
-  if (x<1000) return(NA)
-  year = str_extract(as.character(x), '^\\d{4}')
-  if (is.na(year)) {cat(x,' '); return(NA)}
-  mon = str_extract(as.character(x), '(?<=\\d{4}\\.)\\d{1,2}')
-  if (is.na(mon)) {cat(x,' '); return(paste0(year,'-01-01'))}
-  if (nchar(mon)==1) mon = paste0('0', mon)
-  return(paste0(year,'-',mon,'-','01'))
-}
-replaceNaN <- function(x){
-  x[is.nan(x)] = NA
-  x
+isTRUE <- function(x){
+  !is.na(x) & x
 }
 
-## merge separate data files
-alldata = fread('./data/ukb_data/field_select.csv')
-addf2 = fread('./data/ukb_data/field_select2.csv')
-addf3 = fread('./data/ukb_data/field_select3.csv')
-addf4 = fread('./data/ukb_data/field_select4.csv')
-addf5 = fread('./data/ukb_data/field_select5.csv')
-addf6 = fread('./data/ukb_data/field_select6.csv')
+## extract ICD-10 disease and diagnosis dates
+alldatac = readRDS('ukb_variables.rds')
+ICD10 = alldatac %>% select(contains('41270'))
+ICD10_date = alldatac %>% select(contains('41280'))
+extract_date = alldatac %>% select(eid, sex)
+rm(alldatac); gc()
 
-alldatac = alldata %>%# left_join(eyeimg) %>% 
-  left_join(addf2) %>%  left_join(addf3) %>%  left_join(addf4) %>%  
-  left_join(addf5) %>% left_join(addf6) 
-
+# get disease frequencies stratified by sex
+n_male = sum(extract_date$sex)
+n_female = sum(1 - extract_date$sex)
+regx1 = '^[A-Z]\\d{2}'
+ICD10_2 = ICD10 %>% mutate(across(, ~str_extract(.x, regx1)))
+rm(ICD10); gc()
+alldata = cbind(extract_date, ICD10_2)
+code_male = alldata %>% filter(sex == 1) %>% select(-eid, -sex) %>%
+  unlist() %>% table() %>% data.frame() %>% arrange(desc(Freq))
+code_female = alldata %>% filter(sex == 0) %>% select(-eid, -sex) %>%
+  unlist() %>% table() %>% data.frame() %>% arrange(desc(Freq))
+write.csv(code_male, 'ICD10_code_freq_male.csv', row.names = F)
+write.csv(code_female, 'ICD10_code_freq_female.csv', row.names = F)
 rm(alldata); gc()
-## 40000: date of death;
-## 40001: primary cause of death;
-## 40002: secondary cause of death.
 
-
-
-
-rm(list = ls()); gc()
-
-## clean data
-alldatac = read.csv('data/data_clean_v1.csv')
-colname = names(alldatac)
-
-varset = c('eid', 'X31.0.0', 'X34.0.0', 'X52.0.0', 
-           'X53.0.0', #'X53.1.0',#'X53.2.0','X53.3.0',
-           'X4079.0.0', 'X4079.0.1', #'X4079.1.0', 'X4079.1.1', 'X4079.2.0', 'X4079.2.1', 'X4079.3.0', 'X4079.3.1', 
-           'X4080.0.0', 'X4080.0.1', 
-           'X21001.0.0',
-           'X22032.0.0')
-ukbdata_cleaned = alldatac %>% select(all_of(varset)) 
-names(ukbdata_cleaned) = c('eid', 'sex', 'birth_y', 'birth_m', 
-                'ini_date', #'Rep_date',
-                'DBP1', 'DBP2', 
-                'SBP1', 'SBP2', 
-                'BMI',
-                'IPAQ')
-
-## 31:Sex
-## 34:Year of birth
-## 52:Month of birth
-## 53:Date of attending assessment centre
-## 4079:Diastolic blood pressure, automated reading
-## 4080:Systolic blood pressure, automated reading
-## 21001: BMI
-## 22032: derived MET (Metabolic Equivalent Task) scores data based on IPAQ (International Physical Activity Questionnaire) guidelines
-#age(y) at Ini_date,IPAQ: https://biobank.ndph.ox.ac.uk/showcase/ukb/docs/ipaq_analysis.pdf
-ukbdata_cleaned = ukbdata_cleaned %>% mutate(birth_d = paste0(birth_y, '.', birth_m,'.1'), .after = birth_m) %>% 
-  mutate(birth_d = as.Date(birth_d, format = '%Y.%m.%d')) %>% 
-  mutate(age = (as.Date(ini_date) - birth_d)/365.25, .after = sex) %>% 
-  mutate(age = as.numeric(age))
-mean(is.na(ukbdata_cleaned$age))
-mean(is.na(ukbdata_cleaned$sex))
-#SBP/DBP at Ini_date
-ukbdata_cleaned$DBP = rowMeans(ukbdata_cleaned[,c('DBP1','DBP2')], na.rm = T) %>% 
-  tidyr::replace_na()
-ukbdata_cleaned$SBP = rowMeans(ukbdata_cleaned[,c('SBP1','SBP2')], na.rm = T) %>% 
-  tidyr::replace_na()
-
-#HbA1c
-HbA1c = alldatac %>% select(contains('30750'))
-ukbdata_cleaned$HbA1c = HbA1c[,1]
-#TC/HDL/LDL/TG
-TC = alldatac %>% select(contains('30690'))
-ukbdata_cleaned$TC = TC[,1]
-HDL = alldatac %>% select(contains('30760'))
-ukbdata_cleaned$HDL = HDL[,1]
-LDL = alldatac %>% select(contains('30780'))
-ukbdata_cleaned$LDL = LDL[,1]
-TG = alldatac %>% select(contains('30870'))
-ukbdata_cleaned$TG = TG[,1]
-mean(is.na(HbA1c[,1]))
-mean(is.na(TC[,1]))
-mean(is.na(HDL[,1]))
-mean(is.na(LDL[,1]))
-mean(is.na(TG[,1]))
-
-ukbdata_cleaned <- ukbdata_cleaned %>% 
-  select(-birth_y, -birth_m, -DBP1, -DBP2, -SBP1, -SBP2) 
-
-#edu -3:NA, -7:0, 2-6:1, 
-#1. college or university:2 
-#2.to 6. From A levels to other professional qualifications
-#-7: none of qualifications
-edu = alldatac %>% select(contains('6138.0'))
-eduf = c()
-for (i in 1:nrow(edu)){
-  edui = edu[i, ]
-  edui = edui[!is.na(edui)]
-  if (length(edui)==0 | edui[1] == -3) {eduf[i] = NA; next}
-  edui = sort(edui)
-  if (edui[1] == -7) eduf[i] = 0
-  if (edui[1] == 1) eduf[i] = 2
-  if (edui[1] > 1) eduf[i]=1
+# extract diseases with prevalence < 1%
+code_male = code_male[code_male$Freq >= n_male*0.01, 1] %>% as.character()
+code_female = code_female[code_female$Freq >= n_female*0.01, 1] %>% as.character()
+code2_list = union(code_male, code_female)
+cl <- makeCluster(getOption("cl.cores", 6))
+extract_date = data.frame(extract_date)
+for (k in 1:length(code2_list2)){
+  codei = code2_list2[k]
+  d10_date = ICD10_date
+  d10_date[isTRUE(ICD10_2!=codei)] = NA
+  d10_date = parApply(cl, d10_date, 1, min, na.rm = TRUE)
+  d10_date = as.IDate(d10_date)
+  extract_date[codei] = d10_date
+  rm(d10_date); gc()
+  cat(k,'/ ', length(code2_list2), as.character(Sys.time()), '\n')
+  if (k %% 10 == 0){
+    saveRDS(extract_date, file = 'ukb_icd10_disease_sex.rds')
+  }
 }
-table(eduf, useNA = 'ifany')
-ukbdata_cleaned$edu = eduf
+saveRDS(extract_date, file = 'ukb_icd10_disease_sex.rds')
+stopCluster(cl)
 
-#Ethnic background -3/-1:NA  white(1):0 non-white(>1):1
-ethnic = alldatac %>% select(contains('21000.0'))
-ethnic = ethnic[,1]
-ethnic[which(ethnic == -3 | ethnic == -1)] = NA
-ethnic0 = str_extract(ethnic, '^\\d') %>% as.numeric()
-table(ethnic0, useNA = 'ifany')
-ethnic0[ethnic0 == 1] = 0
-ethnic0[ethnic0 > 1] = 1
-ukbdata_cleaned = ukbdata_cleaned %>% mutate(ethnic = ethnic0)
+# calculate disease prevalence
+ukb_icd10 = readRDS('ukb_icd10_disease_sex.rds')
+ukb_cov = readRDS('ukbdata_v2.rds')
+ukb_icd10 = ukb_cov %>% select(eid, sex) %>% left_join(ukb_icd10)
+icd10 = data.frame(ICD10 = names(ukb_icd10), samples = colSums(!is.na(ukb_icd10)))
+icd10 = icd10[3:nrow(icd10),]
+row.names(icd10) = NULL
+icd10$prevalence = icd10$samples/nrow(ukb_icd10)
+icd10$sample_male = colSums(!is.na(ukb_icd10 %>% filter(sex == 1) %>% select(-eid,-sex)))
+icd10$sample_female = colSums(!is.na(ukb_icd10 %>% filter(sex == 0) %>% select(-eid,-sex)))
+icd10$prevalence_male = icd10$sample_male/sum(ukb_icd10$sex)
+icd10$prevalence_female = icd10$sample_female/sum(1-ukb_icd10$sex)
+icd10 = icd10 %>% filter(prevalence_male>=0.01 | prevalence_female>=0.01) %>% arrange(desc(prevalence))
+write.xlsx(icd10, file = 'disease_prevalence_sex_v4.xlsx')
 
-#smoke  -3:NA 0:0 >0:1
-smoke = alldatac %>% select(contains('1239.0'))
-smoke = smoke[,1]; smoke[which(smoke == -3)] = NA
-table(smoke, useNA = 'ifany')
-smoke[smoke>0] = 1
-ukbdata_cleaned = ukbdata_cleaned %>% mutate(smoke = smoke)
+## merge ICD-10 diagnosis with other variables
+disease_sel = icd10$ICD10
+ukbdata_v3 = ukb_cov %>% select(eid:illmother)
+ukb_icd10 = ukb_icd10 %>% select(eid, all_of(disease_sel))
+ukb_icd10_binary = ukb_icd10 %>% mutate(across(, ~as.numeric(!is.na(.x)))) %>% select(-eid)
+names(ukb_icd10) = c('eid', paste0(disease_sel, 'date'))
+ukb_icd10 = cbind(ukb_icd10, ukb_icd10_binary)
+ukbdata_v3 = ukbdata_v3 %>% left_join(ukb_icd10)
+saveRDS(ukbdata_v3, file = 'ukbdata_v3.rds')
 
-#drink -3:NA 6:0 2-5:1, 1:2
-drink = alldatac %>% select(contains('1558.0'))
-drink = drink[,1]; drink[which(drink == -3)] = NA
-table(drink, useNA = 'ifany')
-drink = factor(drink, levels = 1:6, labels = c(2,1,1,1,1,0))
-ukbdata_cleaned = ukbdata_cleaned %>% mutate(drink = drink)
+## multiple imputation using mice
+ukbdata = readRDS("ukbdata_v3.rds")
+ukbdata = ukbdata %>% mutate(death_date = unlist(death_date))
 
-#sleep -3/-1:NA
-sleep = alldatac %>% select(contains('1160.0'))
-sleep = sleep[,1]; sleep[which(sleep == -3 | sleep == -1)] = NA
-table(sleep, useNA = 'ifany')
-ukbdata_cleaned = ukbdata_cleaned %>% mutate(sleep = sleep)
+# calculate missing percentage
+num_list = c( "age", "tdi", "BMI", "sleep", "phone_use",
+               "DBP", "SBP","HbA1c", "HDL", "LDL", "TG", "calc", "CRP" , 
+               "agedeathmoth","agedeathfath")
+bin_list = c("ethnic", "smoke", "leisure",
+              "longill","illfather","illmother")
+poly_list = c("edu","IPAQ", "drink", "gas")
+map(ukbdata[,c(num_list, bin_list,poly_list)], ~mean(is.na(.)))
+map(ukbdata[,c('ini_date',"death_date")], ~mean(is.na(.)))
 
-#phone_use -3/-1:NA
-phone_use = alldatac %>% select(contains('1120.0'))
-phone_use = phone_use[,1]; phone_use[which(phone_use == -3 | phone_use == -1)] = NA
-table(phone_use, useNA = 'ifany')
-ukbdata_cleaned = ukbdata_cleaned %>% mutate(phone_use = phone_use)
+# impute missing data
+for (sex0 in 0:1){
+  sex_name = ifelse(sex0==0, 'female', 'male')
+  ukbdata_subset = ukbdata %>% filter(sex == sex0)
+  eid = ukbdata_subset$eid
+  # we do not consider disease at baseline due to computational issues.
+  ukbdata_to_impute = ukbdata_subset %>% select(c(num_list, bin_list,poly_list))
+  ukbdata_to_impute = ukbdata_to_impute %>% 
+    mutate_at(num_list, as.numeric)%>%
+    mutate_at(c(bin_list,poly_list), as.factor)
 
-# death date
-death_date = alldatac %>% select(contains('40000.0.0'))
-hist(as.Date(death_date$X40000.0.0), breaks = 'year')
-ukbdata_cleaned <- ukbdata_cleaned %>% mutate(death_date = death_date$X40000.0.0)%>% unlist()
-
-death_cause = alldatac %>% select(contains(c('40001.0.0','40002.0')))
-death_cause[death_cause==""]<-NA
-not_all_na <- function(x) any(!is.na(x))
-not_any_na <- function(x) all(!is.na(x))
-death_cause <- death_cause %>% sapply(., substring, 1, 3) %>% unlist()%>%
-          data.frame %>%
-          select(where(not_all_na))
+  init = mice(ukbdata_to_impute, maxit=0) 
+  meth = init$method
+  meth[num_list] = "pmm" 
+  meth[bin_list] = "logreg" 
+  meth[poly_list] = "polyreg"
   
-names(death_cause) <- paste0("deathcause",1:15)
-
-death.date = alldatac %>% select(contains('40000.0.0'))%>% unlist()
-ukbdeath = alldatac %>% select("eid") %>%
-  mutate(death_date = death.date)
-
-ukbdeath = cbind(ukbdeath, death_cause)
-saveRDS(ukbdeath, file = "data/ukbdeath_v1.rds")
-
-
-## extract disease and diagnosis dates
-ukbdisease = alldatac %>% select("eid") 
-asc <- function(x){strtoi(charToRaw(x),16L)}
-## 65 for the asc II code. 
-## 2600 for the maximal number of ICD10. 
-code2num <- function(x){
-  x1 = substring(x,first=1L,last=1L)
-  x2 = substring(x,first=2L,last=10000L)
-  return((asc(x1)-65)*100+as.numeric(x2))
+  set.seed(123)
+  ukbdata_imputed = mice(ukbdata_to_impute, method = meth, m = 5, maxit = 3)
+  ukbdata_imputed_comp = complete(ukbdata_imputed, action = "long")
+  ukbdata_imputed_comp = ukbdata_imputed_comp %>% mutate(eid = rep(eid,5)) %>% select(-.id)
+  saveRDS(ukbdata_imputed_comp, file = paste0('ukbdata_analysis_comp/ukbdata_imputed_',sex_name,'.rds'))
 }
-num2code <- function(x){
-  a = floor(x/100)
-  x1 = rawToChar(as.raw(a+65))
-  x2 = x-100*a
-  if(x2 >= 10){
-    x2 = as.character(x-100*a)
-    return(paste0(x1,x2))
-  }
-  else{
-    x2 = as.character(x-100*a)
-    return(paste0(x1,'0',x2))
-  }
-}
-ICD10 <- alldatac %>% select(contains('41270'))
-ICD10_date <- alldatac %>% select(contains('41280'))
-N = 2600 # the total number of disease types coded in ICD10
-selectlabel <- c("I10","Z86","Z92","E78","K57","Z87","Z88","K29","R10","R07",
-                 "K21","Z85","K44","Z96","I25","Z53","J45","M19","Z51","Z90",
-                 "E11","H26","K62","M17","N39","I48","E66","Y83","K63","I20")
+rm(list = ls());gc()
 
+# combine imputed datasets with remaining datasets
+ukbdata = readRDS("ukbdata_v3.rds")
+ukbdata = ukbdata %>% mutate(death_date = unlist(death_date))
 
-icd10file <- read_excel('data/disease_prevalence.xlsx')
-selectlabel100 <- icd10file$ICD10
-selectlabel2 <- selectlabel100[!selectlabel100 %in% selectlabel]
-
-for(dcode in selectlabel2){
-  d10 = apply(ICD10, 1, function(x) which(str_detect(x, pattern = dcode)))
-  d10_date = c()
-  for (n in 1:nrow(ICD10)){
-    d10_date[n] = ifelse(length(d10[[n]]) == 0, NA, ICD10_date[n, d10[[n]]] %>% unlist() %>% min())
+cols.var = c("sex", "age", "tdi","edu","ethnic",
+              "BMI","IPAQ", "smoke","drink","sleep","phone_use",
+              "DBP", "SBP","HbA1c", "TC", "HDL", "LDL", "TG","calc","chol","CRP" , 
+              "gas", "leisure",
+              "agedeathmoth", "agedeathfath","longill","illfather","illmother")
+ukbdata = ukbdata %>% select(-cols.var)
+for (sex0 in 0:1){
+  sex_name = ifelse(sex0==0, 'female', 'male')
+  ukbvar_comp = readRDS(paste0('ukbdata_analysis_comp/ukbdata_imputed_',sex_name,'.rds'))
+  ukbdata_subset = ukbvar_comp %>% left_join(.,ukbdata,by = c("eid"))
+  
+  ## set disease states at baseline 
+  icd10file = read_excel('disease_prevalence_sex_v4.xlsx')
+  selectlabel = icd10file$ICD10
+  for(dcode in selectlabel){
+    diseaseDdate = paste0(dcode, "date")
+    ukbdata_subset = ukbdata_subset %>% 
+      mutate_at(c(eval(diseaseDdate),"ini_date"), as.Date, format = "%Y-%m-%d")%>%
+      mutate(diff_datesDI = as.numeric(difftime(get(diseaseDdate), ini_date, units = "days")))%>%
+      mutate(!!paste0(dcode,'_base') := ifelse((is.na(diff_datesDI) | diff_datesDI>=0 ),0,1))
+  }    
+  ukbdata_subset = ukbdata_subset %>% select(-diff_datesDI)
+  labels.base = paste0(selectlabel, "_base")
+  
+  # save the data set for each imputation
+  imp_num = 5
+  for (i in 1:imp_num){
+    ukbdata_subset_i = ukbdata_subset %>% filter(.imp == i)
+    saveRDS(ukbdata_subset_i, file = paste0('ukbdata_analysis_comp/ukbdata_imputed_',sex_name,'_', i, '.rds'))
   }
-  d10[sapply(d10, length) > 0 ] <- 1
-  d10[sapply(d10, length) ==0 ] <- 0
-  temp <- as.data.frame(cbind(unlist(d10),d10_date))
-  names(temp) <- c(dcode, paste0(dcode, "date"))
-  ukbdisease <- cbind(ukbdisease,temp)
+  gc()
 }
 
-ukbdata <- merge(ukbdata_cleaned,ukbdisease,by="eid")
 
-saveRDS(ukbdata, file = "data/ukbdata_v1.rds")
+
+
 
 
 
--- main analysis/1. pairwise analysis with tmle.R
+++ main analysis/1. pairwise analysis with tmle.R
@@ -1,15 +1,16 @@
+# This is an example for a female disease.
+# The code for male diseases should be modified with the corresponding data.
 library(dplyr)
 library(glmnet)
 library(doParallel)
-
+setwd("~/")
 glmnet.control(epsnr = 1e-5, mxitnr = 25)
-detectCores(logical = F)
-lr_tmle <- function(dcode2){
+
+lr_tmle <- function(dcode2, diffAY = 0){
   diseaseAdate <- paste0(dcode1, "date")
   diseaseYdate <- paste0(dcode2, "date")
   # select the subpopulation for studying the effects of diseaseA on disease Y
   ukbdata_pairwise <- ukbdata_i %>% 
-    # mutate_at(c(eval(diseaseAdate),eval(diseaseYdate),"ini_date"), as.Date, format = "%Y-%m-%d")%>%
     mutate(diff_datesAY = as.numeric(difftime(get(diseaseYdate), get(diseaseAdate), units = "days")),
            diff_datesAI = as.numeric(difftime(get(diseaseAdate), ini_date, units = "days")),
            diff_datesYI = as.numeric(difftime(get(diseaseYdate), ini_date, units = "days")))%>%
@@ -18,8 +19,21 @@ lr_tmle <- function(dcode2){
     # exclude the people who had diseaseY prior to the baseline 
     filter(is.na(diff_datesYI) | diff_datesYI>=0 )%>%
     # include the people who did not have diseaseY prior to diseaseA after baseline
-    filter(is.na(diff_datesAY) | diff_datesAY>=0 )%>%
+    filter(is.na(diff_datesAY) | diff_datesAY>=diffAY )%>%
     select(all_of(c(cols.x,dcode1,dcode2)), diff_datesAY)
+  
+  ## Step 0. Prefit the propensity score model and exclude individuals with extreme values
+  A <- ukbdata_pairwise[,dcode1] %>% as.numeric()
+  X <- ukbdata_pairwise %>% select(all_of(cols.x))
+  X_dummy = model.matrix(~., X)[,-1]
+  X_dummy = as(X_dummy, "dgCMatrix")
+  g <- glmnet(X_dummy, A, family = binomial(), lambda = 0, lower.limits = -8, upper.limits = 8, maxit = 200, thresh = 1e-6)
+  if (g$jerr<0) return(c(dcode1, dcode2, rep(NA, 9)))
+  g_w = as.vector(predict(g, s = 0, newx = X_dummy, type = 'response'))
+  g_min = min(g_w[A == 1])
+  g_max = max(g_w[A == 0])
+  ukbdata_pairwise = ukbdata_pairwise %>% filter(g_w >= g_min & g_w <= g_max)
+  rm(X_dummy, X); gc()
   
   minyears <- round(min(ukbdata_pairwise$diff_datesAY,na.rm = TRUE)/365.2425,3)
   maxyears <- round(max(ukbdata_pairwise$diff_datesAY,na.rm = TRUE)/365.2425,3)
@@ -27,42 +41,43 @@ lr_tmle <- function(dcode2){
   sp_num <- nrow(ukbdata_pairwise)
   
   if(sp_num <= 3){
-    cat("Number of sample size is samller than 3 in the scenario of",dcode1,"to",dcode2)
-    continue
+    cat("Number of sample size is smaller than 3 in the scenario of",dcode1,"to",dcode2)
+    return(c(dcode1, dcode2, sp_num, rep(NA, 8)))
   }
   
-  ## Step 1. Learn the outcome model
-  col.treat_out <- c(dcode1,dcode2)
-  Y <- ukbdata_pairwise[,col.treat_out[2]]%>% as.numeric()
-  A <- ukbdata_pairwise[,col.treat_out[1]]%>% as.numeric()
-  X <- ukbdata_pairwise %>% select(all_of(cols.x)) %>% select(-all_of(paste0(col.treat_out, '_base')))
+  ## Step 1. Learn the propensity score model
+  Y <- ukbdata_pairwise[,dcode2]%>% as.numeric()
+  A <- ukbdata_pairwise[,dcode1]%>% as.numeric()
+  X <- ukbdata_pairwise %>% select(all_of(cols.x))
   rm(ukbdata_pairwise); gc()
-
   
-  X_A_dummy = model.matrix(~., cbind(A, X))[,-1]
-  Q <- glmnet(X_A_dummy, Y, family = binomial(), lambda = 0, maxit = 50, thresh = 1e-6)
-  if (Q$jerr<0) return(rep(NA, 9))
-  coefA = coef(Q, s = 0)['A',1]
-  Q_A_link = as.vector(predict(Q, s = 0, newx = X_A_dummy))
-  Q_A = plogis(Q_A_link)
-  Q_1 = plogis(Q_A_link + (1-A)*coefA)
-  Q_0 = plogis(Q_A_link - A*coefA)
-  rm(X_A_dummy); gc()
-  
-  ## Step 2. Learn the propensity score model
   X_dummy = model.matrix(~., X)[,-1]
-  g <- glmnet(X_dummy, A, family = binomial(), lambda = 0, maxit = 50, thresh = 1e-6)
-  if (g$jerr<0) return(rep(NA, 9))
+  X_dummy = as(X_dummy, "dgCMatrix")
+  g <- glmnet(X_dummy, A, family = binomial(), lambda = 0, lower.limits = -8, upper.limits = 8, maxit = 200, thresh = 1e-6)
+  if (g$jerr<0) return(c(dcode1, dcode2, sp_num, rep(NA, 8)))
   g_w = as.vector(predict(g, s = 0, newx = X_dummy, type = 'response'))
   H_1 <- 1/g_w
   H_0 <- -1/(1-g_w) # Pr(A=0|X) is 1-Pr(A=1|X)
   H_A <- ifelse(A == 1, H_1, H_0) # if A is 1 (treated), assign H_1, else H_0
   rm(X_dummy); gc()
   
+  ## Step 2. Learn the outcome model
+  X_A_dummy = model.matrix(~., cbind(A, X))[,-1]
+  X_A_dummy = as(X_A_dummy, "dgCMatrix")
+  Q <- glmnet(X_A_dummy, Y, family = binomial(), lambda = 0, lower.limits = -8, upper.limits = 8, thresh = 1e-6)
+  if (Q$jerr<0) return(c(dcode1, dcode2, sp_num, rep(NA, 8)))
+  coefA = coef(Q, s = 0)['A',1]
+  Q_A_link = as.vector(predict(Q, s = 0, newx = X_A_dummy))
+  Q_A = pmax(pmin(plogis(Q_A_link), 1 - 1e-6), 1e-6)
+  Q_1 = pmax(pmin(plogis(Q_A_link + (1-A)*coefA), 1 - 1e-6), 1e-6)
+  Q_0 = pmax(pmin(plogis(Q_A_link - A*coefA), 1 - 1e-6), 1e-6)
+  rm(X_A_dummy); gc()
+  
   ## Step 3. Estimate the flunctuation parameter model
   glm_fit <- glm(Y ~ -1 + offset(qlogis(Q_A)) + H_A, family=binomial)
   eps <- coef(glm_fit)
-  
+  minmax = max(abs(glm_fit$linear.predictors))
+  extreme_value = ifelse(minmax>30, 1, 0)
   ## Step 4. Estimate the initial parameters
   Q_A_update <- plogis(qlogis(Q_A) + eps*H_A)
   Q_1_update <- plogis(qlogis(Q_1) + eps*H_1)
@@ -78,62 +93,59 @@ lr_tmle <- function(dcode2){
   conf_high <- tmle_ate + 1.96*tmle_se
   pval <- 2 * (1 - pnorm(abs(tmle_ate / tmle_se)))
   
-  estimates <- round(c(tmle_ate, conf_low, conf_high,pval),3)
-  update.estimates <- c(dcode1, dcode2,estimates,minyears,medyears,maxyears)
+  estimates <- round(c(tmle_ate, conf_low, conf_high,pval),5)
+  update.estimates <- c(dcode1, dcode2, sp_num, estimates,
+                        minyears, medyears, maxyears, extreme_value)
   return(update.estimates)
 }
 
 ############################################
 ## set disease states at baseline 
-icd10file <- readxl::read_excel('disease_prevalence_v2.xlsx')
+icd10file <- readxl::read_excel('disease_prevalence_sex_v4.xlsx')
 selectlabel <- icd10file %>% 
-  filter(!stringr::str_detect(ICD10, '[O-P]|[R-T]|[V-Z]\\d{2}')) %>% 
-  filter(prevalence >= 0.01) %>% pull(ICD10)
+  filter(!stringr::str_detect(ICD10, '[Q-T]|[V-Y]\\d{2}')) %>% 
+  filter(prevalence_female >= 0.01) %>% pull(ICD10)
 
 ############################################
-## format output
-dnum = length(selectlabel); #number of diseases under consideration
-CORRmat <- data.frame(matrix(NA,dnum,dnum+1))
-TMLEmat <- data.frame(matrix(NA,0, 9))
-
-names(CORRmat) <- c("expo",selectlabel)
-CORRmat$expo <- selectlabel
-names(TMLEmat) <- c("expo","outcome","mean","low95ci","up95ci","pvalue","mininterval","medinterval","maxinterval")
-
-############################################
-## estimation
-cols.var <- c("sex", "age", "tdi","edu","ethnic",
+cols.var <- c("age", "tdi","edu","ethnic",
               "BMI","IPAQ", "smoke","drink","sleep","phone_use",
               "DBP", "SBP","HbA1c", "HDL", "LDL", "TG","calc","CRP" , 
-              "gas",
-              "leisure",
+              "gas", "leisure",
               "agedeathmoth", "agedeathfath","longill","illfather","illmother")
-
 cols.disease.base <- paste0(selectlabel, "_base")
-cols.x <- c(cols.var,cols.disease.base)
+cols.x <- c(cols.var, cols.disease.base)
+
+selectlabel <- selectlabel[!stringr::str_detect(selectlabel, 'Z\\d{2}')] # remove ZXX 
+dnum <- length(selectlabel) # number of diseases under consideration
 
 ## do for each of the five imputed datasets
 imp_num = 5
-
+path = 'main_analysis/'
 for (i in 1:imp_num){
-  ## Uncomment below for the final analysis in the server
-  ukbdata_i = readRDS(paste0('ukbdata_analysis_comp/ukbdata_imputed_', i, '.rds'))
+  ukbdata_i = readRDS(paste0('ukbdata_analysis_comp/ukbdata_imputed_female_', i, '.rds'))
   
-  cl <- makeCluster(getOption("cl.cores", 4))
+  ## format output
+  CORRmat <- data.frame(matrix(NA,dnum,dnum+1))
+  TMLEmat <- data.frame(matrix(NA,0, 11))
+  names(CORRmat) <- c("expo", selectlabel)
+  CORRmat$expo <- selectlabel
+  names(TMLEmat) <- c("expo","outcome","sample_size","mean","low95ci","up95ci","pvalue","mininterval","medinterval","maxinterval","extreme_value")
+  
+  ## estimation
+  cl <- makeCluster(getOption("cl.cores", 10)) # memory limit
   registerDoParallel(cl)
-  
   for (dcode1 in selectlabel){
     cat(sprintf("imp:%d treat:(%d / %d)", i, match(dcode1, selectlabel), dnum), as.character(Sys.time()), '\n')
-    
-    selectlabel2 <-  selectlabel[! selectlabel %in% c(dcode1)]
-    tmle_rlt = foreach(dcode2=selectlabel2, .packages=c('glmnet','dplyr'),
-                       .combine = rbind) %dopar% lr_tmle(dcode2)
-    colnames(tmle_rlt) = c("expo","outcome","mean","low95ci","up95ci","pvalue","mininterval","medinterval","maxinterval")
-    TMLEmat = rbind(TMLEmat, tmle_rlt, make.row.names = FALSE)
+    selectlabel2 <- selectlabel[!selectlabel %in% c(dcode1)]
+    tmle_rlt <- foreach(dcode2=selectlabel2, .packages=c('glmnet','dplyr'),
+                       .combine = rbind) %dopar% lr_tmle(dcode2, diffAY = 0)
+    colnames(tmle_rlt) <- c("expo","outcome","sample_size","mean","low95ci","up95ci","pvalue","mininterval","medinterval","maxinterval","extreme_value")
+    TMLEmat <- rbind(TMLEmat, tmle_rlt, make.row.names = FALSE)
     CORRmat[CORRmat$expo == dcode1, selectlabel2] <- tmle_rlt[,'mean']
     
-    write.csv(TMLEmat, file = paste0('results/pairwise_estimates_imp',i,'.csv'))
-    write.csv(CORRmat, file = paste0('results/pairwise_correlation_matrix_imp',i,'.csv'))
+    write.csv(TMLEmat, file = paste0(path,'results_female/pairwise_estimates_imp',i,'.csv'))
+    write.csv(CORRmat, file = paste0(path,'results_female/pairwise_correlation_matrix_imp',i,'.csv'))
+    gc()
   }
   stopCluster(cl)
 }
